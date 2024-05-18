6958f819ec60fd718c719342fb700db0256298fc
jmorla <user.name>
2024-05-18 16:48:13 -0400

    first commit

--- generator/compile.sh
+++ generator/compile.sh
@@ -2,4 +2,4 @@ echo "Compiling ReportProcessor and Generate classes"
 javac com/jmorla/ReportProcessor.java com/jmorla/Generate.java -d out
 
 echo "Running processor"
-javac -cp target/ -processor com.jmorla.ReportProcessor com/jmorla/Company.java
+javac -cp target/ -processor com.jmorla.ReportProcessor com/jmorla/Company.java -d target/
